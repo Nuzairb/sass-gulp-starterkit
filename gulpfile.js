dd189d0761d350f073b8c076b5e2edec770f6263
Nuzair Ibrahim <nuzairb@gmail.com>
2018-11-07 14:45:05 +0500

    init

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,10 @@
 var gulp = require('gulp');
+
+var styleSRC = './src/scss/style.css';
+var styleDIST = './dist/css/';
+
+gulp.task('styles', function() {
+  gulp.src( styleSRC )
+    .pipe( rename( {suffix: 'min '} ) )
+    .pipe( gulp.dist( styleDIST ) );
+});
