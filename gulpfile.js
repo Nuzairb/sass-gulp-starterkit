f98b87c4142ca5e4071a684e05243296ef81f082
Nuzair Ibrahim <nuzairb@gmail.com>
2018-11-07 15:05:11 +0500

    git commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
+var rename = require('gulp-rename');
 
 var styleSRC = './src/scss/style.css';
 var styleDIST = './dist/css/';
 
-gulp.task('styles', function() {
+gulp.task('style', function() {
   gulp.src( styleSRC )
-    .pipe( rename( {suffix: 'min '} ) )
-    .pipe( gulp.dist( styleDIST ) );
+    .pipe( rename( {suffix: '.min'} ) )
+    .pipe( gulp.dest( styleDIST ) );
 });
